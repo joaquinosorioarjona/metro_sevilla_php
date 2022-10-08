286f12bbac8240a1f97a02ba25a511003cf1d1fb
Joaquin Osorio <joaosoarj@hotmail.com>
2022-10-08 18:23:13 +0200

    Update mapa.php

--- mapa.php
+++ mapa.php
@@ -29,7 +29,7 @@
 			font: 10pt "Helvetica Neue", Arial, Helvetica, sans-serif;
 		}#map {
 			width: 100%;
-			height: 900px; 
+			height: 850px; 
 			box-shadow: 5px 5px 5px #888;
 		}
 		.form-control{
@@ -43,44 +43,41 @@
 			text-align: center;
 			height: 3%;
 		}
-			.container {
-		display: flex;
-	}
-	.leaflet-control {
-		margin: 0px 5px;
-	}
-	.leaflet-control-layers-expanded {
-		background: #CD5C5C none repeat scroll 0 0;
-		color: #FFFFFF;
-		font: 15px Calibri, sans-serif;
-		padding: 6px 10px 6px 6px;
-	}
-	.legend {
-		background: #CD5C5C none repeat scroll 0 0;
-		color: #FFFFFF;
-		font: 15px Calibri, sans-serif;
-		padding: 6px;		
-		line-height: 18px;
-	} 
-	.legend i{
-		width: 18px;
-		height: 18px;
-		float: left;
-		margin-right: 2px;
-		opacity: 0.7;
-	}
-	.sidebar-header{
-		background-color: #CD5C5C;
-		color: #000000;
-	}
-	.sidebar-tabs > li.active, .sidebar-tabs > ul > li.active {
-        color: #fff;
-        background-color: #CD5C5C; 
-	}
-	.lorem {
-		font-style: italic;
-		color: #666666;
-	}
+		.leaflet-control {
+			margin: 0px 5px;
+		}
+		.leaflet-control-layers-expanded {
+			background: #CD5C5C none repeat scroll 0 0;
+			color: #FFFFFF;
+			font: 15px Calibri, sans-serif;
+			padding: 6px 10px 6px 6px;
+		}
+		.legend {
+			background: #CD5C5C none repeat scroll 0 0;
+			color: #FFFFFF;
+			font: 15px Calibri, sans-serif;
+			padding: 6px;		
+			line-height: 18px;
+		} 
+		.legend i{
+			width: 18px;
+			height: 18px;
+			float: left;
+			margin-right: 2px;
+			opacity: 0.7;
+		}
+		.sidebar-header{
+			background-color: #CD5C5C;
+			color: #FFFFFF;
+		}
+		.sidebar-tabs > li.active, .sidebar-tabs > ul > li.active {
+			color: #fff;
+			background-color: #CD5C5C; 
+		}
+		.lorem {
+			font-style: italic;
+			color: #666666;
+		}
 	</style>
 </head>
 <body>
@@ -117,6 +114,8 @@
 		<ul role="tablist">
 			<li><a href="#home" role="tab"><i class="fa fa-info"></i></a></li>
 			<li><a href="#planos" role="tab"><i class="fa fa-map"></i></a></li>
+			<li><a href="#poblacion" role="tab"><i class="fa fa-users"></i></a></li>
+			<li><a href="#fuentes" role="tab"><i class="fa fa-link"></i></a></li>
 			<li><a href="https://www.youtube.com/watch?v=xx5t_-hmbQg" role="tab"><i class="fa fa-youtube"></i></a></li>
 			<li><a href="https://es.wikipedia.org/wiki/Metro_de_Sevilla" role="tab"><i class="fa fa-wikipedia-w"></i></a></li>
 		</ul>
@@ -130,18 +129,74 @@
 			<p class="lorem">El Área Metropolitana de Sevilla es una de las aglomeraciones urbanas con menor desarrollo de su trazado ferroviario, traduciendose en una gran falta de conectividad entre zonas, mayores tiempos de desplazamiento de los ciudadanos a sus lugares de trabajo o residencia, una gran congestión de tráfico rodado y niveles insostenibles de contaminación y polución.</p>
 			<p class="lorem">A día de hoy, la red de Metro de Sevilla solo cuenta con la línea 1 de Metro. A corto plazo solo hay planes de realizar el trazado norte de la Línea 3, mientras que la Línea 2 corre peligro de quedar congelada otros 20 años. Este mapa no solo muestra la línea completa de Metro, sino que recoge la propuesta de SevillaQuiereMetro de conectar las líneas 1 y 2 en Nervión y Santa Justa para aumentar así la interconectividad entre zonas. El mapa también recoge la línea completa de Metrocentro, que se extendería de Santa Justa a la Encarnación para volver a Plaza Nueva y convertirla en una línea circular que puede favorecer la movilidad turística. </p>
 			<p class="lorem">Además, el mapa incorpora las propuestas de Tranvías de Dos Hermanas y Alcalá, corrobora la importancia de la creación de la estación de Cercanías de Casilla de los Pinos, y elabora una simplificación de la Línea de Tranvía del Aljarafe, teniendo en cuenta la propuesta de expansión de la Línea 2 de Metro a Ginés, y la elaboración de dos ramales de la Línea 2 de Cercanías que conducirían a Bormujos y La Puebla del Río. De este modo, se busca no solo favorecer la movilidad entre los pueblos del Aljarafe y Sevilla, sino también entre ellos mismos. Además, se propone una cuarta línea de tranvía que conecte el Hospital San Lázaro con la estación de Cercanías de San Jerónimo y llegue hasta la Rinconada, conectando la Algaba.</p>
-			<p class="lorem">Por último, se propone una mejora de la red de Cercanías basada en la propuesta de de la asociación para la Igualdad y la Mejora del Transporte (Apimt), pero unificando líneas para aumentar la competitividad y el flujo de viajeros. De este modo, además de la expansión de la Línea 2 y su ramal al Aljarafe, se propone la conexión directa entre el centro de Sevilla y el Aeropuerto (el cual no cuenta actualmente con ninguna conexión ferroviaria con la ciudad), la creación de una línea que lleve a Alcalá de Guadaira y Carmona, y la transformación de la línea de Media Distancia Utrera-Marchena-Osuna a una línea de Cercanías. Finalmente, se propone que la línea a Alcalá de Guadaira sea una línea semicircular que la conecte tanto con Sevilla como Dos Hermanas, siendo clave para ello la estación existente de Pio Palmete.</p>
+			<p class="lorem">Por último, se propone una mejora de la red de Cercanías basada en la propuesta de de la Asociación para la Igualdad y la Mejora del Transporte (Apimt), pero unificando líneas para aumentar la competitividad y el flujo de viajeros. De este modo, además de la expansión de la Línea 2 y su ramal al Aljarafe, se propone la conexión directa entre el centro de Sevilla y el Aeropuerto (el cual no cuenta actualmente con ninguna conexión ferroviaria con la ciudad), la creación de una línea que lleve a Alcalá de Guadaira y Carmona, y la transformación de la línea de Media Distancia Utrera-Marchena-Osuna a una línea de Cercanías. Finalmente, se propone que la línea a Alcalá de Guadaira sea una línea semicircular que la conecte tanto con Sevilla como Dos Hermanas, siendo clave para ello la estación existente de Pio Palmete.</p>
 		</div>
 		<div class="sidebar-pane" id="planos">
 			<h1 class="sidebar-header">Planos actuales<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
 			<p><a href="images/plano_metro.jpg"><img src="images/plano_metro.jpg" alt="Plano del Metro de Sevilla.jpg"width="400" height="300"></a>
 			<p><a href="images/plano_cercanias.png"><img src="images/plano_cercanias.png" alt="Plano del Cercanías de Sevilla.png"width="400" height="500"></a>
 		</div>
+		<div class="sidebar-pane" id="poblacion">
+		<h1 class="sidebar-header">Población<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
+		<h3>El Área Metropolitana de Sevilla cuenta con una población de aproximadamente 1.548.741 habitantes según datos del INE de 2021.</h3>
+		<br>
+		<h5>Sevilla (total: 756.026 habitantes)</h5>
+		<ul><li>Distrito Este-Alcosa-Torreblanca: 153.678 habitantes</li>
+			<li>Distrito Cerro Amate: 90.433 habitantes</li>
+			<li>Distrito Sur: 75.620 habitantes</li>
+			<li>Distrito Macarena: 74.576 habitantes</li>
+			<li>Distrito Norte: 71.963 habitantes</li>
+			<li>Distrito San Pablo-Santa Justa: 66.600 habitantes</li>
+			<li>Distrito Casco Histórico: 60.437 habitantes habitantes</li>
+			<li>Distrito Nervión: 51.578 habitantes</li>
+			<li>Distrito Triana: 48.948 habitantes</li>
+			<li>Distrito Bellavista-La Palmera: 35.785 habitantes</li>
+			<li>Distrito Los Remedios: 27.009 habitantes</li>
+		</ul>
+		<br>
+		<h5>Dos Hermanas (total: 133.964 habitantes)</h5>
+			<ul><li>Dos Hermanas centro: 96.931 habitantes</li>
+			<li>Montequinto: 35.698 habitantes</li>
+			<li>Fuente del Rey: 1.285 habitantes</li>
+			<li>Marismas y Puntales-Adriano: 50 habitantes</li>
+			<li>Entrenúcleos: en construcción</li>
+		</ul>
+		<br>
+		<h5>Alcalá de Guadaira: 75.546 habitantes</h5>
+		<br>
+		<h5>Conurbación del Aljarafe: (total: 249.113 habitantes)</h5>
+			<ul><li>Mairena del Aljarafe: 46.555 habitantes</li>
+			<li>Coria del Río: 30.908 habitantes</li>
+			<li>Camas: 27.560 habitantes</li>
+			<li>Tomares: 25.455 habitantes</li>
+			<li>Bormujos: 22.180 habitantes</li>
+			<li>San Juan de Aznalfarache: 21.774 habitantes</li>
+			<li>Castilleja de la Cuesta: 17.516 habitantes</li>
+			<li>Gines: 13.428 habitantes</li>
+			<li>Gelves: 10.183 habitantes</li>
+			<li>Palomares del Río: 8.843 habitantes</li>
+			<li>Santiponce: 8.538 habitantes</li>
+			<li>Valencina de la Concepción: 7.776 habitantes</li>
+			<li>Salteras: 5.564 habitantes</li>
+			<li>Castilleja de Guzmán: 2.833 habitantes</li>
+			</ul>
+		<br>
+		<h5>La Rinconada: 39.204 habitantes</h5>
+		<h5>La Algaba: 16.503 habitantes</h5>
+
+		</div>
+		<div class="sidebar-pane" id="fuentes">
+			<h1 class="sidebar-header">Fuentes<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
+			<p><a href="https://sevillaquieremetro.org/">Sevilla Quiere Metro</a>
+			<p><a href="http://www.sevilla21.es/">Sevilla21</a>
+			<p><a href="https://sevillasemueve.org/">Sevilla se Mueve</a>
+			<p><a href="https://apimt.wordpress.com/">Asociación para la Igualdad y la Mejora del Transporte</a>
+		</div>
 	</div>
 	</div>
 	
 
-	<div id="map"></div>
+	<div id="map" class="sidebar-map"></div>
 	<script>
 	<?php include 'lista-lineas.php'; ?>
 	//Create and append the options
@@ -154,29 +209,22 @@
 	}
 	
 	var map = L.map('map').setView([37.378944, -5.980278], 12);
-	
-	var sidebar = L.control.sidebar('sidebar').addTo(map);
-	
+		
 	var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; OpenStreetMap contributors'
-	}).addTo(map);
+	});
 	var maptile = L.mapboxGL({
         attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        style: 'https://api.maptiler.com/maps/6516ccba-253a-4f2f-8920-b2828eb7d858/style.json?key=Y9KJbkXF4ObUWmvEpf6s'
+        style: 'https://api.maptiler.com/maps/d7ae625d-6061-4ee2-803c-1b07fbe1b864/style.json?key=Y9KJbkXF4ObUWmvEpf6s'
       });	
 	var carto = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 		subdomains: 'abcd',
 		maxZoom: 20
-	});		
-	var stamen = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		subdomains: 'abcd',
-		minZoom: 1,
-		maxZoom: 16,
-		ext: 'jpg'
-	});
+	}).addTo(map);	
+
+	var sidebar = L.control.sidebar('sidebar').addTo(map);
 
 	var celdas = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
 		layers: 'metrosevilla:celdas_movilidad',
@@ -193,7 +241,7 @@
 	
 	var metro1 = new L.GeoJSON(lineas, {
 		filter: metro1filter,
-		weight: 4,
+		weight: 6,
 		color: "#008000",
 		onEachFeature: popup_lineas,
 	});
@@ -203,7 +251,7 @@
 	
 	var metro2 = new L.GeoJSON(lineas, {
 		filter: metro2filter,
-		weight: 4,
+		weight: 6,
 		color: "#0000FF",
 		onEachFeature: popup_lineas,
 	});
@@ -213,7 +261,7 @@
 	
 	var metro3 = new L.GeoJSON(lineas, {
 		filter: metro3filter,
-		weight: 4,
+		weight: 6,
 		color: "#FF0000",
 		onEachFeature: popup_lineas,
 	});
@@ -223,7 +271,7 @@
 	
 	var metro4 = new L.GeoJSON(lineas, {
 		filter: metro4filter,
-		weight: 4,
+		weight: 6,
 		color: "#FFFF00",
 		onEachFeature: popup_lineas,
 	});
@@ -235,7 +283,7 @@
 
 	var metrocentro = new L.GeoJSON(lineas, {
 		filter: metrocentrofilter,
-		weight: 2,
+		weight: 4,
 		color: "#40E0D0",
 		onEachFeature: popup_lineas
 	}).addTo(map);
@@ -245,7 +293,7 @@
 	
 	var tranvia = new L.GeoJSON(lineas, {
 		filter: tranviafilter,
-		weight: 2,
+		weight: 4,
 		color: "#A0522D",
 		onEachFeature: popup_lineas
 	}).addTo(map);
@@ -255,7 +303,7 @@
 	
 	var cercanias = new L.GeoJSON(lineas, {
 		filter: cercaniasfilter,
-		weight: 2,
+		weight: 4,
 		color: "#8B008B",
 		onEachFeature: popup_lineas
 	}).addTo(map);
@@ -347,10 +395,9 @@
 	window.onload = makeRequest("Linea 1 de Metro");
 	
 	var baseLayers = {
+		"CARTO": carto,
 		"OpenStreetsMap": osm,
 		"Maptile": maptile,
-		"CARTO": carto,
-		"Stamen": stamen
 	};
 	
 	var overlays = {
@@ -386,6 +433,8 @@
 	}).addTo(map);	
 	
 	</script>
-
+	<div style=text-align:right>
+		<p style=color:white><strong>Autor: Joaquín Osorio Arjona (Doctor en Geografía, Investigador en la Universidad de Sevilla. <a href=”url”>www.joaquinosorioarjona.com</a></strong></p>	
+	</div>
 </body>
 </html>
